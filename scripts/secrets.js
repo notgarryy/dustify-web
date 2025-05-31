bbd4c0200437c662739d4ba2ddb7ac1d91cd2aaf
notgarryy <garrynelson456@gmail.com>
2025-05-31 12:01:05 +0700

    v0.1

---
+++ scripts/secrets.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCciQ3izTkFzB-9Yc0Mf6tiURFPXypJO8",
+    authDomain: "app-dustify.firebaseapp.com",
+    projectId: "app-dustify",
+    storageBucket: "app-dustify.firebasestorage.app",
+    messagingSenderId: "971567482172",
+    appId: "1:971567482172:web:e3768d3c35e8b57219ca39",
+    measurementId: "G-EWMRHFGKWQ"
+};
+
+export { firebaseConfig };
